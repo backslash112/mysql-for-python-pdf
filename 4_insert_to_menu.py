3d41bac305b1917516d0999f78111e1d92624c47
backslash112 <yangcun@live.com>
2017-01-19 09:18:00 +0800

    p75 Using user-defined variables

--- 4_insert_to_menu.py
+++ 4_insert_to_menu.py
@@ -1,9 +1,13 @@
 import MySQLdb as mysql
+import sys
 
 mydb = mysql.connect(host="localhost",
                            user="root",
                            passwd="123456",
                            db="menu")
 cur = mydb.cursor()
-statement = """insert into fish(name, price) values("shark", "13.00")"""
+fish = sys.argv[1]
+price = sys.argv[2]
+statement = """insert into fish(name, price) values('%s', %s)""" % (fish, price)
+
 cur.execute(statement)
