28779bee2237cf6dcd5ebb79f6d8a8824524a596
backslash112 <yangcun@live.com>
2017-01-31 11:06:10 +0800

    Understand the substring() and insert() methods

--- capfirst.sql
+++ capfirst.sql
@@ -10,9 +10,12 @@ declare outstring varchar(1000) default lower(instring);
 
 WHILE i <= char_length(instring) do
 SET achar = substring(instring, i, 1);
+-- substring(value, position, length), position start with 1, not 0
 SET imark = CASE WHEN i = 1 then ' '
 else substring(instring, i - 1, 1) end;
-IF imark IN (' ', '&', '''', '_', '?', ';', ':', '!', ',', '-', '/', '(', '.') then set outstring = insert(outstring, i, 1, upper(achar));
+IF imark IN (' ', '&', '''', '_', '?', ';', ':', '!', ',', '-', '/', '(', '.')
+then set outstring = insert(outstring, i, 1, upper(achar));
+-- insert(value, position, length, new_value), position start with 1, not 0
 end if;
 set i = i + 1;
 end WHILE;
